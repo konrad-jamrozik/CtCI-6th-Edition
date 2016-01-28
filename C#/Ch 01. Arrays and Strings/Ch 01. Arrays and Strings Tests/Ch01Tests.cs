799e6f37612ea6f00e36d1ed62c9ebb930aa7451
Konrad Jamrozik <spawarotti@jamro.pl>
2016-01-28 18:56:58 +0100

    add C# Ch01Q01 solution and tests

---
+++ C#/Ch 01. Arrays and Strings/Ch 01. Arrays and Strings Tests/Ch01Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Ch_01.Arrays_and_Strings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ch_01.Arrays_and_Strings_Tests
+{
+    [TestClass]
+    public class Ch01Tests
+    {
+        [TestMethod]
+        public void Q1Test()
+        {
+            AssertCorrect(Q1.A1);
+            AssertCorrect(Q1.A2);
+        }
+
+        private static void AssertCorrect(Func<string, bool> predicate)
+        {
+            Assert.AreEqual(true, predicate(""));
+            Assert.AreEqual(true, predicate("abc"));
+            Assert.AreEqual(false, predicate("aa"));
+            Assert.AreEqual(true, predicate("abc"));
+            Assert.AreEqual(false, predicate("abcc"));
+            Assert.AreEqual(false, predicate("aabc"));
+            Assert.AreEqual(false, predicate("abbc"));
+            Assert.AreEqual(false, predicate("abcdea"));
+        }
+    }
+}
--- C#/Ch 01. Arrays and Strings/Ch 01. Arrays and Strings/Q1.cs
+++ C#/Ch 01. Arrays and Strings/Ch 01. Arrays and Strings/Q1.cs
@@ -9,23 +9,49 @@ namespace Ch_01.Arrays_and_Strings
 {
     public class Q1
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            
         }
 
+        // Time complexity: O(min(n,c)) where c is the asciiCharsetSize
+        // Space complexity: O(n + c) // n for the input string c for the dictionary
         public static bool A1(String s)
         {
             Contract.Requires(s != null);
             Contract.Requires(s.Length >= 0);
 
+            var asciiCharsetSize = 128;
+            if (s.Length > asciiCharsetSize) return false;
+
             // preconditons: s != null, s >= 0
-            var dict = new Dictionary<char, char>(s.Length);
+            var dict = new Dictionary<char, char>(asciiCharsetSize);
             foreach (char c in s)
             {
                 if (dict.ContainsKey(c))
                     return false;
                 else
                     dict.Add(c, c);
+            }
+            return true;
+        }
+
+        // Time complexity: O(n log(n))
+        // Space complexity: O(n) // n for the input string
+        public static bool A2(String s)
+        {
+            Contract.Requires(s != null);
+            Contract.Requires(s.Length >= 0);
+
+            var asciiCharsetSize = 128;
+            if (s.Length > asciiCharsetSize) return false;
+
+            var chars = s.ToCharArray();
+            Array.Sort(chars);
+            for (var i = 0; i < chars.Length - 1; i++)
+            {
+                if (chars[i] == chars[i + 1])
+                    return false;
             }
             return true;
         }
