ef40a3cac557008c54a28b808d9b9b217041477c
Konrad Jamrozik <spawarotti@jamro.pl>
2016-01-28 15:21:58 +0100

    Create C# dir with .gitignore + add programs
    C# warm-up programs: HelloWorld and FizzBuzz

---
+++ C#/WarmUp/WarmUp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using static System.Linq.Enumerable;
+
+namespace WarmUp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            HelloWorld();
+            FizzBuzz();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.Out.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        ///     Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz”
+        ///     instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples
+        ///     of both three and five print “FizzBuzz”.
+        ///     http://c2.com/cgi/wiki?FizzBuzzTest
+        /// </summary>
+        private static void FizzBuzz()
+        {
+            foreach (var i in Range(1, 100))
+            {
+                var s = new StringBuilder();
+
+                if (i%3 == 0)
+                    s.Append("Fizz");
+                if (i%5 == 0)
+                    s.Append("Buzz");
+
+                if (s.Length == 0)
+                    Console.Out.WriteLine(i);
+                else
+                    Console.Out.WriteLine(s.ToString());
+            }
+        }
+    }
+}
