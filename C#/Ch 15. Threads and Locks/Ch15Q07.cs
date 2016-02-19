3bad0f55e91a1a6218fd0590288848830d81b617
Konrad Jamrozik <spawarotti@jamro.pl>
2016-02-19 21:00:17 +0100

    add Ch15Q07 solution.

---
+++ C#/Ch 15. Threads and Locks/Ch15Q07.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ch_15.Threads_and_Locks
+{
+    [TestClass]
+    public class Ch15Q07
+    {
+        [TestMethod]
+        public void TestCh15Q07()
+        {
+
+        }
+
+        public static void Main()
+        {
+            // thread 1: generate numbers
+            // thread 2: print fizz if divisible by 3
+            // thread 3: print buzz if divisible by 5
+            // thread 4: print fizzbuzz if divisible by 15
+            // Only one thread at a time can have access to console
+            // fizzbuzz has to be checked any of fizz and buzz
+
+            // Idea:
+            // Thread that generates numbers is a producer that writes to a queue consumed by fizzbuzz. After fizzbuzz processes the output, it pushes to queue read by buzz, which then passes to fizz.
+
+            // pseudocode:
+            // Start thread one generating numbers with 100 ms break. The thread will put constantly to Q1.
+            // Start fizzbuzz thread constantly reading from Q1, printing if necessary, and pushing to Q2.
+            // And so on. Last thread doesn't push anywhere.
+            // Problem: before buzz processes and prints out previous number, fizzbuzz might process and print out next number. To prevent it, processing next numbers should require more resources:
+            // fizzbuzz: 1, fizz: 2, buzz: 3, fizzbuzz: 4, fizz: 5, buzz: 6, fizzbuzz: 7, etc. Sempahore, I guess.
+
+            Console.Out.WriteLine("Running main");
+            
+            var gn = new Thread(GenerateNumbersClass.GenerateNumbers);
+            gn.Start();
+
+            
+            new Thread(PrintFizzBuzz).Start();
+            new Thread(PrintFizz).Start();
+            new Thread(PrintBuzz).Start();
+            new Thread(PrintNumber).Start();
+
+            
+            Console.ReadKey();
+        }
+
+        internal static readonly ConcurrentQueue<int> FizzBuzzInputQueue = new ConcurrentQueue<int>();
+        private static readonly ConcurrentQueue<int> FizzInputQueue = new ConcurrentQueue<int>();
+        private static readonly ConcurrentQueue<int> BuzzInputQueue = new ConcurrentQueue<int>();
+        private static readonly ConcurrentQueue<int> NumberInputQueue = new ConcurrentQueue<int>();
+
+        public static readonly Semaphore Sem1 = new Semaphore(1, 1);
+        public static readonly Semaphore Sem2 = new Semaphore(0, 1);
+        public static readonly Semaphore Sem3 = new Semaphore(0, 1);
+        public static readonly Semaphore Sem4 = new Semaphore(0, 1);
+
+        private static void PrintFizzBuzz()
+        {
+            while (true)
+            {
+                int currentNumber;
+                if (FizzBuzzInputQueue.TryDequeue(out currentNumber))
+                {
+                    Sem1.WaitOne();
+                    if (currentNumber % 3 == 0 && currentNumber % 5 == 0)
+                        Console.Out.WriteLine("FizzBuzz");
+                    else
+                        FizzInputQueue.Enqueue(currentNumber);
+                    Sem2.Release();
+                }
+            }
+        }
+
+        private static void PrintFizz()
+        {
+            while (true)
+            {
+                Sem2.WaitOne();
+                int currentNumber;
+                if (FizzInputQueue.TryDequeue(out currentNumber))
+                {                    
+                    if (currentNumber % 3 == 0)
+                        Console.Out.WriteLine("Fizz");
+                    else
+                        BuzzInputQueue.Enqueue(currentNumber);                    
+                }
+                Sem3.Release();
+            }
+        }
+
+        private static void PrintBuzz()
+        {
+            while (true)
+            {
+                Sem3.WaitOne();
+                int currentNumber;
+                if (BuzzInputQueue.TryDequeue(out currentNumber))
+                {
+                    if (currentNumber % 5 == 0)
+                        Console.Out.WriteLine("Buzz");
+                    else
+                        NumberInputQueue.Enqueue(currentNumber);
+                    
+                }
+                Sem4.Release();
+            }
+        }
+
+        private static void PrintNumber()
+        {
+            while (true)
+            {
+                Sem4.WaitOne();
+                int currentNumber;
+                if (NumberInputQueue.TryDequeue(out currentNumber))
+                {
+                    Console.Out.WriteLine(currentNumber);
+                    
+                }
+                Sem1.Release();
+            }
+        }
+    }
+
+    class GenerateNumbersClass
+    {
+        internal static void GenerateNumbers()
+        {
+            int currentNumber = 1;
+
+            while (!Stop)
+            {
+                if (currentNumber == 1000)
+                    Stop = true;
+                Ch15Q07.FizzBuzzInputQueue.Enqueue(currentNumber++);
+
+            }
+        }
+
+        public static bool Stop { get; set; }
+    }
+}
---
+++ C#/Scratchpad/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Scratchpad
+{
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+        }
+    }
+}
